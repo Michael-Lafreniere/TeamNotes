e461cce49224de7ced827a00ec4fc2c289266869
Michael Lafreniere <temike1@gmail.com>
2020-03-24 11:13:25 -0700

    Moved QueryComments into the Query.js file from Note.js

--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-// import Query from '../utils/Query';
+import React, { useState } from 'react';
+import { QueryComments } from '../utils/Query';
 
 import './Note.css';
 import './Comment.css';
@@ -8,34 +8,9 @@ const Comment = ({ data: comment }) => {
   return <div className="comment">{comment}</div>;
 };
 
-function QueryComments(noteID) {
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetch('http://localhost:5000/comments', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ noteID: noteID })
-      })
-        .then(res => res.json())
-        .catch(error => setError(error));
-      setData(data);
-      setLoading(false);
-    };
-    fetchData();
-  }, [noteID]);
-
-  return { data, loading, error };
-}
-
 const Note = noteData => {
   const [note] = useState(noteData.data);
   const data = QueryComments(note._id);
-
-  // console.table('data:', data.data);
 
   let comments;
   if (data !== undefined && data.data !== undefined) {
--- client/src/utils/Query.js
+++ client/src/utils/Query.js
@@ -20,3 +20,26 @@ export default function Query(url) {
 
   return { data, loading, error };
 }
+
+export function QueryComments(noteID) {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch('http://localhost:5000/comments', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ noteID: noteID })
+      })
+        .then(res => res.json())
+        .catch(error => setError(error));
+      setData(data);
+      setLoading(false);
+    };
+    fetchData();
+  }, [noteID]);
+
+  return { data, loading, error };
+}
