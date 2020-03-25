08960775fbb5a5d92ad59c889587a3ed64211956
Michael Lafreniere <temike1@gmail.com>
2020-03-25 16:26:31 -0700

    Started to add a NewNote component, along with a CSS for it.  Does not currently submit any data.

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,23 @@ import './App.css';
 import './components/NoteFeed';
 import NoteFeed from './components/NoteFeed';
 
+const NewNote = () => {
+  return (
+    <div className="new-note">
+      <input placeholder="New note title" />
+      <textarea rows="5" placeholder="Start a new note here" />
+      <button>Submit</button>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1 className="title">Team Notes</h1>
       </header>
+      <NewNote />
       <NoteFeed />
     </div>
   );
--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { QueryData } from '../utils/Query';
 
 import './Comment.css';
