c12f436c02445e18681bec745ca8cfca81be2fc6
Michael Lafreniere <temike1@gmail.com>
2020-03-27 16:27:47 -0700

    Working on resolving an error in newNote.js

--- client/src/components/NewNote.js
+++ client/src/components/NewNote.js
@@ -8,14 +8,19 @@ const initalState = {
   body: ''
 };
 
+const postNote = (user, title, body) => {
+  console.log(user, title, body);
+};
+
 const newNoteReducer = (state, action) => {
   switch (action.type) {
     case 'title':
-      return { ...state, title: action.value };
+      return { ...state, title: action.title };
     case 'body':
       return { ...state, body: action.value };
     case 'send':
-      if (action.user !== undefined) {
+      if (action.user !== undefined && action.title.length > 0) {
+        postNote(action.user, action.title, action.boly);
       }
       return { state };
     default:
@@ -28,7 +33,6 @@ const NewNote = () => {
   const [state, dispatch] = useReducer(newNoteReducer, initalState);
   const titleRef = React.createRef();
   const bodyRef = React.createRef();
-  const { title, body } = state;
 
   return (
     <div className="new-note">
@@ -36,17 +40,17 @@ const NewNote = () => {
         <input
           ref={titleRef}
           placeholder="New note title"
-          value={title}
-          onChange={() => {
-            dispatch({ type: 'title', value: titleRef.current.value });
+          value={state.title}
+          onChange={event => {
+            dispatch({ type: 'title', title: titleRef.current.value });
           }}
         />
         <textarea
           ref={bodyRef}
           rows="5"
           placeholder="Start a new note here"
-          value={body}
-          onChange={() => {
+          value={state.body}
+          onChange={event => {
             dispatch({ type: 'body', value: bodyRef.current.value });
           }}
         />
